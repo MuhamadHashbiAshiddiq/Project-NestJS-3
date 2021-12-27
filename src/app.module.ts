df7fb6f0c1c194c7c09d621fba227d85f931c4b2
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-27 13:20:31 +0700

    a: App config and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppIndoService } from './app.indo.service';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppDummy } from './app.dummy';
 import { AppService } from './app.service';
@@ -8,13 +9,14 @@ import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'nest-events',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Event],
       synchronize: true,
     }),
