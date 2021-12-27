da3d5de627544037d88e099605f33418cdc830f6
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-27 15:25:29 +0700

    a: Query builder introduction

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventsService } from './evennts.service';
 import { Attendee } from './attendee.entity';
 import { Event } from './event.entity';
 import { EventsController } from './events.controller';
@@ -7,5 +8,6 @@ import { EventsController } from './events.controller';
 @Module({
   imports: [TypeOrmModule.forFeature([Event, Attendee])],
   controllers: [EventsController],
+  providers: [EventsService],
 })
 export class EventsModule {}
