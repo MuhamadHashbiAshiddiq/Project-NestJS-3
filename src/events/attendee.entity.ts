11c056c00efba6a3da744798a29a83ea633dfcb9
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-27 14:13:57 +0700

    a: One to many relation

---
+++ src/events/attendee.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Event } from './event.entity';
+
+@Entity()
+export class Attendee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Event, (event) => event.attendees, {
+    nullable: false,
+  })
+  @JoinColumn()
+  event: Event;
+}
