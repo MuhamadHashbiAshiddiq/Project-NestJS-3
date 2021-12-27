afc8462953c64c6bb1fa4b72e21fa77aa7c48788
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-27 11:16:08 +0700

    a: Validation groups and options

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -54,7 +54,9 @@ export class EventsController {
   }
 
   @Post()
-  async create(@Body(ValidationPipe) input: CreateEventDto) {
+  async create(
+    @Body(new ValidationPipe({ groups: ['create'] })) input: CreateEventDto,
+  ) {
     return await this.repository.save({
       ...input,
       when: new Date(input.when),
@@ -62,7 +64,10 @@ export class EventsController {
   }
 
   @Patch(':id')
-  async update(@Param('id') id, @Body() input: UpdateEventDto) {
+  async update(
+    @Param('id') id,
+    @Body(new ValidationPipe({ groups: ['update'] })) input: UpdateEventDto,
+  ) {
     const event = await this.repository.findOne(id);
 
     return await this.repository.save({
