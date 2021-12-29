18bc5c9f8828679d06d44e1ae303428c57cdcac8
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-29 11:45:43 +0700

    a: Relations between entities

--- src/events/attendee.entity.ts
+++ src/events/attendee.entity.ts
@@ -1,4 +1,3 @@
-import { Expose } from 'class-transformer';
 import {
   Column,
   Entity,
@@ -7,6 +6,8 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Event } from './event.entity';
+import { Expose } from 'class-transformer';
+import { User } from 'src/auth/user.entity';
 
 export enum AttendeeAnswerEnum {
   Accepted = 1,
@@ -30,10 +31,19 @@ export class Attendee {
   @JoinColumn()
   event: Event;
 
+  @Column()
+  eventId: number;
+
   @Column('enum', {
     enum: AttendeeAnswerEnum,
     default: AttendeeAnswerEnum.Accepted,
   })
   @Expose()
   answer: AttendeeAnswerEnum;
+
+  @ManyToOne(() => User, (user) => user.attended)
+  user: User;
+
+  @Column()
+  userId: number;
 }
